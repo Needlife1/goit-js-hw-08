b090932b1f9abbb8c9ee619b768a87cc08d1bcb9
Needlife1 <needlife124@gmail.com>
2023-02-21 16:29:35 +0200

    all is ready

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
 // Add imports above this line
+import Simplelightbox from "simplelightbox";
 import { galleryItems } from './gallery-items';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
-console.log(galleryItems);
+
+
+
+
+const galleryRaf = document.querySelector('.gallery');
+const gallaryMarkup = createGallery(galleryItems);
+
+function createGallery (galleryItems) {
+return galleryItems.map(({preview, original, description}) => {
+return `<a class="gallery__item" href="${original}">
+  <img 
+  class="gallery__image" 
+  src="${preview}" 
+  alt="${description}" />
+</a>`
+}).join(''); 
+};
+
+galleryRaf.insertAdjacentHTML('beforeend', gallaryMarkup);
+
+var lightbox = new Simplelightbox('.gallery a', { 
+     captionsData: "alt",
+    captionDelay: 250,
+ });
